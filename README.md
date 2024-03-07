200df16e54aa65f7c256da84fb25d11b29e9f91d
appollo30 <leo.lasnier@me.com>
2024-03-07 23:40:55 +0100

    ajout de README.md

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # R_Dataviz_Project
+# Le Tour de France en Chiffres
+
+## Noureddine BOUDALI
+## Léo LASNIER
+
+## Sommaire
+1. User Guide
+2. Developper Guide
+
+## 1. User guide
+
+Ce projet est le projet de fin de l'unité 'DSIA_4101C R et DataViz'.
+Il a pour sujet les différentes édition du Tour de France. Les données sont extraites de https://www.kaggle.com/datasets/pablomonleon/tour-de-france-historic-stages-data , elles présentent la Grande Boucle sur 3 fichiers csv différents, qui représentent l'épreuve à différentes échelles. 
+Le premier, ```tdf_winners.csv```, représente des données sur les 106 premières éditions du Tour, de 1903 à 2019. Il contient notamment des données sur les gagnants de chacune des éditions, avec le temps de parcours, l'écart avec le second au classment général, le nombre d'étapes avec le maillot jaune, etc. 
+Le second, ```tdf_stages.csv```, contient autant d'observations que d'étapes, et contient les lieux de départ et d'arrivée des différentes étapes, le vainqueur du jour, le type de l'étape, etc.
+Le troisième, ```stage_data.csv```, contient des informations plus précises sur l'étape, comme les différents participants à l'étape et leur rang à l'arrivée.
+
+Il faut donc croiser certaines données entre les 3 datasets, car la donnée n'est pas uniformisée entre les 3, le code pour nettoyer les données et les croiser est commenté dans la première partie "Global" du code.
+
+Pour pouvoir géolocaliser les départs et arrivées d'étapes, on a également eu recours au dataset ```communes_departement_regions.csv``` de https://www.data.gouv.fr/fr/datasets/communes-de-france-base-des-codes-postaux/. Ce dataset contient notamment les coordonnées GPS des communes en France ainsi que leurs noms. On a donc du le croiser avec ```tdf_stages``` pour obtenir les géolocalisations des départs et arrivées d'étapes.
+
+### 1.1 Déploiement
+
+Pour déployer le projet, il faut d'abord le cloner sur la machine:
+```git clone git@github.com:appollo30/R_Dataviz_Project.git```
+
+Puis ouvrir le fichier ```app.R``` dans RStudio.
+
+Ensuite, pour chacun des packages inclus dans le fichier ```requirements.txt```, les installer à l'aide de la commande ```install.packages(<PACKAGE EN QUESTION>)``` dans le terminal de RStudio.
+
+Enfin, lancer le dashboard en entrant la commande ```shiny::runApp()``` dans le terminal RStudio ou bien en cliquant sur 'Run App'.
+
+Alternativement, vous pouvez aller dans ```File > Open Project```, puis ouvrir le fichier ```R_dataviz_Project.Rproj```. Puis lancer ```shiny::runApp()```.
+
+### 1.2 Les données
+
+Au lancement de l'application, on voit 3 graphiques.
+
+#### 1-Les distances des étapes
+Le premier graphique montre un histogramme représentant la distribution des distances des étapes selon le type d'étape parmi:
+-Les contre-la-montre individuels
+-Les étapes plates
+-Les étapes de moyenne montagne
+-Les étapes de haute montagne
+-Les contre-la-montre en montagne
+-Les contre-la-montre par équipes
+
+On peut sélectionner le manière dont la distribution est représentée à l'aide des boutons de radio en haut, d'une part en histogramme, d'autre part en boite à moustaches. 
+On peut également sélectionner quels types d'étapes on souhaite voir dans le graphique à l'aide des checkbox. 
+
+On constate deux catégories d'étapes, les étapes en ligne et les contre-la-montre. Les étapes en ligne tournent autour des 200km, tandis que les CLM tournent plutôt à 75km. C'est normal, car dans une étape en ligne, on court avec le peloton et on peut donc être dans l'aspiration de celui-ci. Il faut noter que quand on est protégé du vent, c'est jusqu'à 40% de l'effort qui est économisé. Donc les étapes en lignes sont conçues pour être plus longues que les CLM, car la majorité de l'effort fourni par les coureurs est économisé, et l'énergie consommée lors d'une étape en ligne est comparable à un CLM, même si ce dernier est beaucoup moins long.
+Par ailleurs, les CLM par équipes sont plus longs que les individuels pour cette même raison, car les équipiers se relaient pour protéger les autres du vent.
+
+#### 2-Données géolocalisées
+On peut voir les données géolocalisées sur 2 cartes auxquelles on peut accéder avec les boutons. 
+La première visualisation montre les nombres de départs et d'arrivées des étapes du Tour. Selon le nombre de passages de la Grande Boucle, le point est plus ou moins large. On peut avoir les données plus précises en cliquant dessus pour obtenir le nom de la villes, et le nombre de passages.
+La seconde carte montre les Types des étapes. Les contre-la-montres sont affichés en nuances de bleu (en fonction du type de CLM) et les étapes en lignes vont du vert au rouge en fonction du dénivelé. On peut également voir les données précises de chaque étape en cliquant sur le point.
+
+On distingue pour les départs/arrivées les différentes grandes métropoles de France qui ont un point plus grand que les autres, car le Tour passe fréquemment dans les grande villes. À noter que les données sont biaisées car certaines arrivées du Tour arrivent dans des grands cols, qui ne sont pas renseignés dans ```communes_departements_regions```, donc pour les grands cols récurrents du tour, on devrait avoir des gros points à ces endroits-là. Par exemple, il est indiqué que le Tour est arrivé 29 fois à l'Alpe d'Huez, qui n'est pas renseigné par ```communes_departements_regions```. 
+On peut également souligner qu'il pourrait être intéressant d'avoir le liste des montées/cols par lesquels passent chaque étape (une étape de montagne n'a pas nécessairement une arrivée au sommet, c'est même souvent l'inverse).
+Pour les types d'étapes, les étapes de montagne coïncident avec les massifs montagneux Français, tels que les Vosges, le Jura, les Alpes, le Massif central et les Pyrénées.
+
+#### 3-Vitesses moyennes
+On a un scatterplot des temps de parcours du vainqueur chaque édition, en fonction de la distance. On peut sélectionner les années des éditions qu'on souhaite étudier. On semble distinguer une relation linéaire (logique, car en principe à vitesses égales le temps et la distance sont proportionnels) , donc on trace également une régression linéaire entre les deux variables. On peut également voir les vitesses moyennes de la sélection. 
+On remarque tout d'abord qu'il y a deux éditions très courtes en distance par rapport aux autres, et qui ont été complétées en un temps anormalement grand pour la distance (25.5 km/h de moyenne). Il s'agit des deux premières éditions du Tour en 1903 et 1904. La raison de cette faible vitesse est liée à la longueur des étapes, en effet, le Tour comptait alors seulement 6 étapes, en comparaison avec les 21 d'aujourd'hui, mais ces étapes étaient extrèmement longues et excédaient presque toutes les 350 km (avec par exemple la titanesque étape Paris-Lyon, 467km) https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Tour_de_France_1903_schema.png/300px-Tour_de_France_1903_schema.png. Ces étapes sont donc si longues qu'il est facile d'imaginer que les coureurs ne savaient pas gérer leur effort car le Tour était alors le seul grand Tour à étapes. Par la suite, les éditions ont augmenté le nombre d'étapes (15 en 1913) et raccourci la longueur des étapes, qui tournaient plus vers les 250km. 
+On remarque en bougeant le slider que même s'il y a une relation linéaire entre la distance et le temps, la vitesse moyenne du Tour n'a eu de cesse d'augmenter (27km/h en 1913 et 40km/h en 2019). De plus, on remarque que la distance totale des Tours était à son pic dans les années 1915-1925, avec plus de 5000 km (sachant qu'on était encore à des Tours à 15 étapes), tandis qu'au 21eme siècle elles sont entre 3300 et 3700km.
+
+## 2. Developper guide
+
+À la racine du projet on peut trouver:
+-Le fichier du code ```App.R```
+-Le répertoire ```archive``` qui contient les 4 fichiers csv utilisés pour le projet
+-Le fichier ```requirements.txt``` qui contient tous les packages additionnels
+-Le fichier ```README.md```
+-Le fichier ```R_Dataviz_Project.Rproj```
+
+Le code est structuré en 3 parties, GLOBAL, UI et SERVER. 
+
+#### GLOBAL
+La partie GLOBAL est chargée de nettoyer les données et de les croiser entre elles
+
+#### UI 
+UI contient 3 zones, une pour chaque graphique et est chargée de l'organisation de l'affichage du dashboard.
+
+Petite spécification par rapport à la carte, on utilise le package Leaflet, qui utilise les données Openstreetmap pour afficher des données géolocalisées.
+
+#### SERVER
+SERVER se charge du rendu des visualisations et des interactions entre les inputs et les outputs. 
+L'interaction input-output se fait via des ```reactiveValues``` qui sont des variables qui peuvent changer au cours de l'exécution de l'app. Elles sont stockées dans ```v```. Pour les changer, on utilise des ```observeEvent``` sur chaque élément d'input. 
+À l'intérieur de ces ```observeEvent```, on définit comment les ```reactiveValues``` changent.
+
+Donc pour les maps, on a une variable ```CurrentMap``` qui vaut soit 1 soit 2 selon la carte choisie.
+Et on choisit de faire le rendu de l'une ou de l'autre car les 2 sont stockées dans des variables statiques.
+
+Pour l'Histogramme/Boxplot, on a 2 variables ```reactiveValues```, ```radio_buttons_selection``` qui contient une string, soit ```"Histogram"```, soit ```"Boxplot"``` et qui désigne le type de graphique que l'on souhaite afficher, et ```data_distances``` qui désigne les types d'étapes que l'on souhaite afficher.
+
+Pour le scatterplot, on a ```sliderData``` qui contient le maximum et le minimum du slider. On effectue ensuite un filtre pour les éditions se déroulant entre le min et le max.
---
+++ app.R
@@ -0,0 +1,287 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(shiny)
+library(leaflet)
+library(ggthemes)
+
+##GLOBAL
+
+##Lecture de communes
+#(pour la geoloc)
+communes_data <- read_csv("archive/communes_departement_region.csv")
+communes_data <- select(communes_data, c("nom_commune_complet","latitude","longitude"))
+communes_data <- communes_data %>% mutate(nom_commune_complet = str_squish(str_remove_all(nom_commune_complet, "[0-9]")))
+communes_data <- communes_data %>% distinct(nom_commune_complet, .keep_all = TRUE)
+#On enleve les caracteres numeriques de communes_data , ainsi Paris 12 devient Paris, 
+#On supprime ensuite les villes qui apparaissent plusieurs fois, puisque Paris 12 et Paris 13 deviennent Paris
+
+##Lecture de stage_data
+stage_data <- read.csv("archive/stage_data.csv")
+#On utilise read.csv car le csv contient des apostrophes, qui ne sont pas gerees par read.table
+stage_data <- as_tibble(stage_data)
+colnames(stage_data)[3] <- "stage_id"
+stage_data <- stage_data %>% mutate(stage_id = paste(year,stage_id,sep="-"), rank = as.numeric(rank), team = na_if(team,""))
+#Il peut paraitre judicieux d'identifier chaque etape par un code de la forme
+#"<Annee de l'edition>-stage-<Numero d'etape>
+stage_data <- stage_data %>% select(-c(bib_number,elapsed,time))
+#On supprime le numero de dossard
+
+switch_names <- function(full_name) {
+  words <- str_split(full_name, " ")[[1]]
+  switched_name <- paste(c(words[length(words)],words[-length(words)]), collapse = " ")
+}
+
+stage_data <- stage_data %>% mutate(rider = sapply(rider,switch_names))
+#Les noms dans stage_data sont stockes sous la forme "Nom Prenom", alors que 
+#dans stages et winners c'est "Prenom Nom". On change donc le format dans
+#stage_data. A noter que ce n'est pas satisfaisant pour les noms composes
+#qui sont separes par un espace, exemple " Serrano Marcos Antonio". On ne sait
+#pas s'il s'appelle Antonio Serrano Marcos  ou Marcos Antonio Serrano. Je
+#vous laisse imaginer pour les coureurs Flamands, qui sont nombreux, car
+#le cyclisme est un sport très populaire en Flandres. Exemple: quel est le vrai
+#nom de Van der Poel Mathieu, le champion du monde 2023 de course en ligne?
+#Ici on optera pour une structure "Prenom Nom", avec la possibilite d'avoir des noms composes et non des prenoms
+#Ainsi, Pointet Jean Marc s'appellera Marc Pointet Jean, mais Van Aert Wout
+#s'appellera Wout Van Aert
+stage_data <- stage_data %>% mutate(rider = as_factor(rider))
+
+#Lecture de tdf_winners
+
+winners <- read.csv("archive/tdf_winners.csv")
+winners <- select(winners, c("edition","start_date","winner_name","distance","time_overall","time_margin","stage_wins","stages_led","age","born","nationality"))
+winners <- as_tibble(winners)
+
+winners <- mutate(winners,start_date= year(as.Date(start_date)), winner_name = as_factor(winner_name), nationality = as_factor(nationality), time_margin = time_margin*60)
+colnames(winners)[2] <- "Year"
+colnames(winners)[6] <- "Time_Margin_Minutes"
+#On utilise as.Date car la date de depart de l'edition etait stockee comme <chr>
+#Pour des raisons de simplicite, On utilise desormais l'annee de l'edition a la place du jour de depart de l'epreuve (a part dans des cas rarissimes, le Tour commence fin juin-debut juillet tous les ans)
+
+
+##Lecture de tdf_stages
+stages <- read.csv("archive/tdf_stages.csv")
+stages <- as_tibble(stages)
+
+stages <- mutate(stages, Date = as.Date(stages$Date))
+
+stages <- filter(stages,!str_detect(Winner, "Cancelled"))
+#Petite subtilite pour certaines etapes qui se sont vues annuler, on doit donc
+#filtrer les apparitions du mot "cancelled"
+
+stages <- mutate(stages, stage_id = paste(year(Date),"stage",Stage,sep="-"))
+#De meme que pour stage_data, on utilise une id d'etape
+
+stages <- mutate(stages, Winner = if_else(stages$Type == "Team time trial", NA,Winner),Winner_Country = if_else(stages$Type == "Team time trial" | Winner_Country == "",NA,Winner_Country))
+#On a une subtilite avec les victoires d'etapes ici: dans la grande majorite des
+#cas, les etapes sont classiques et individuelles (un seul gagnant pour l'etape)
+#En revanche, il existe un type d'etape bien specifique: le contre-la-montre par
+#equipes. Ici, ce n'est pas un coureur qui gagne la course mais bien une equipe entiere.
+#Ainsi, on supprime le gagnant de l'etape lorsqu'il s'agit d'un CLM par equipes (dans le csv c'est l'equipe victorieuse qui est renseignee)
+simplified_Type <- function(old_Type) {
+  if (old_Type == "High mountain stage" || old_Type == "Mountain stage" || old_Type == "Stage with mountain(s)" || old_Type == "Mountain Stage" || old_Type == "Stage with mountain"){
+    simplified_Type <- "Mountain stage"
+  }
+  else if (old_Type == "Flat cobblestone stage" || old_Type == "Transition stage" || old_Type == "Flat Stage" || old_Type == "Flat stage" || old_Type == "Plain stage" || old_Type == "Half Stage" || old_Type == "Intermediate stage" || old_Type == "Plain stage with cobblestones"){
+    simplified_Type <- "Flat stage"
+  }
+  else if (old_Type == "Hilly stage" || old_Type == "Medium mountain stage"){
+    simplified_Type <- "Medium mountain stage"
+  }
+  else simplified_Type <- old_Type
+}
+stages <- stages %>% 
+  mutate(Type = sapply(Type,simplified_Type))
+
+
+stages <- left_join(stages, communes_data, by = c("Origin" = "nom_commune_complet"))
+stages <- stages %>% rename(lat_origin = latitude, long_origin = longitude)
+stages <- left_join(stages, communes_data, by = c("Destination" = "nom_commune_complet"))
+stages <- stages %>% rename(lat_destination = latitude, long_destination = longitude)
+#Pour avoir les coordonnees gps des departs et arrivees des etapes, on merge communes_data et
+#stages. On eut avoir des problemes pour les etapes ne se deroulant pas en France (il y en a en general
+#une ou deux par edition), ou bien pour les arrivees
+stages <- mutate(stages, Origin = as_factor(Origin), Destination = as_factor(Destination), Type = as_factor(Type),Winner = as_factor(Winner), Winner_Country = as_factor(Winner_Country))
+
+
+Origins <- data.frame(table(stages$Origin)) %>%
+  rename(nom_commune = Var1, departs = Freq)
+Destinations <- data.frame(table(stages$Destination)) %>%
+  rename(nom_commune = Var1, arrivees = Freq)
+
+communes <- full_join(Origins, Destinations, by = "nom_commune")
+rm(Origins)
+rm(Destinations)
+communes[is.na(communes$departs), "departs"] <- 0
+communes[is.na(communes$arrivees), "arrivees"] <- 0
+communes <- left_join(communes, communes_data, by = c("nom_commune" = "nom_commune_complet"))
+
+# UI
+ui <- fluidPage(
+  titlePanel("Projet DSIA_4101C: Le Tour de France en Chiffres"),
+  verticalLayout(
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons(
+          "rb_distance_plot",
+          "Affichage",
+          choices = c("Histogram","Boxplot")
+        ),
+        checkboxGroupInput(
+          "Checkbox_stages",
+          "Types d'Étapes",
+          levels(stages$Type),
+          selected = levels(stages$Type),
+          inline = FALSE
+        )
+      ),
+      mainPanel(
+        plotOutput("distHistogram")
+      )
+    ),
+    
+    titlePanel("Carte des Étapes"),
+    sidebarLayout(
+      sidebarPanel(
+        actionButton("ButtonMap1", "Départs/Arrivées"),
+        actionButton("ButtonMap2", "Type d'Étape")
+      ),
+      mainPanel(
+        leafletOutput("Map1")
+      )
+    ),
+    
+    titlePanel("Vitesses moyennes des éditions"),
+    verticalLayout(
+      mainPanel(
+        plotOutput("Scatterplot")
+      ),
+      sidebarPanel(
+        sliderInput("Slider",
+                    "Années",
+                    min=min(winners$Year),
+                    max=max(winners$Year),
+                    value=c(min(winners$Year),max(winners$Year)),
+                    step=1,
+                    sep = " ",
+                    animate = TRUE)
+      )
+    )
+  )  
+)
+
+
+# SERVER
+server <- function(input, output) {
+  
+  v <- reactiveValues(CurrentMap=1,
+                      data_distances = levels(stages$Type),
+                      sliderData = winners %>%
+                        select(c("distance","time_overall")),
+                      radio_buttons_selection = "Histogram")
+  
+#Cartes  
+  map1 <- leaflet() %>%
+    addCircleMarkers(data = communes,
+                     popup = paste(sep = "<br/>",
+                                   communes$nom_commune,
+                                   paste("Départs: ",communes$departs," - ","Arrivées: ",communes$arrivees,sep="")
+                     ),
+                     color = "navy",
+                     radius = (communes$departs + communes$arrivees)/10
+    ) %>%
+    addTiles() %>%
+    setView(lng = 2.4, lat = 46.53, zoom = 5)  
+  
+  map2 <- leaflet() %>%
+    addCircleMarkers(data = stages,
+                     lat = stages$lat_destination,
+                     lng = stages$long_destination,
+                     popup = paste(sep = "<br/>",
+                                   paste("Date: ", stages$Date),
+                                   paste("Départ: ",stages$Origin,", Arrivée: ", stages$Destination),
+                                   paste("<b>Type:",stages$Type,"<b>")
+                     ),
+                     color = ifelse(stages$Type == "Individual time trial","cyan",ifelse(stages$Type == "Mountain time trial","#100681",ifelse(stages$Type == "Team time trial","#8AABEB",ifelse(stages$Type == "Flat stage","#84BD1B",ifelse(stages$Type == "Medium mountain stage","#FCA50AFF",ifelse(stages$Type == "Mountain stage","#D02E10","cyan")))))),
+                     stroke = TRUE,
+                     fillOpacity = 0.5
+                     
+    ) %>%
+    addTiles() %>%
+    setView(lng = 2.4, lat = 46.53, zoom = 5)  
+  
+  observeEvent(input$ButtonMap1, {
+    v$CurrentMap <- 1
+  })
+  
+  observeEvent(input$ButtonMap2, {
+    v$CurrentMap <- 2
+  })
+  
+  output$Map1 <- renderLeaflet({
+    if (v$CurrentMap == 1) map1
+    else map2
+  })
+  
+  
+#Histogramme/Boxplot
+  observeEvent(input$rb_distance_plot, {
+    v$radio_buttons_selection <- input$rb_distance_plot
+  })
+  
+  observeEvent(input$Checkbox_stages, {
+    v$data_distances <- input$Checkbox_stages
+  })
+  
+  output$distHistogram <- renderPlot({
+    if (v$radio_buttons_selection == "Histogram") {
+      filter(stages, Type %in% v$data_distances) %>% 
+        ggplot(aes(x=Distance,fill = Type)) +
+        geom_histogram(binwidth = 10)+
+        labs(x = "Distance (km)", y = "Fréquence", title="Distance des étapes")+
+        theme_economist()+
+        scale_fill_economist()
+    }
+    else {
+      filter(stages, Type %in% v$data_distances) %>% 
+        ggplot(aes(x = Type, y=Distance, fill = Type)) +
+        geom_boxplot(outlier.shape = NA)+
+        labs(y = "Distance (km)", title="Distance des étapes")+
+        theme_economist()+
+        scale_fill_economist()
+    }
+  })
+  
+  
+#Scatterplot 
+  observeEvent(input$Slider,{
+    v$sliderData <- winners %>%
+      filter(Year >= input$Slider[1] & Year <= input$Slider[2] & !is.na(time_overall)) %>%
+      select(c("distance","time_overall"))
+  })
+  
+  output$Scatterplot <- renderPlot({
+    v$sliderData %>%
+      ggplot(aes(x=distance,y=time_overall)) +
+      geom_point()+
+      geom_smooth(method="lm",formula = "y ~ x")+
+      labs(x = "Distance (km)", y = "Temps de parcours (km)", title = "Distances et temps de parcours", subtitle = paste("Vitesse moyenne des éditions: ", mean(v$sliderData$distance/v$sliderData$time_overall), "km/h"))+
+      theme_economist() +
+      scale_fill_economist()
+    
+    
+  })
+}
+
+shinyApp(ui,server)
